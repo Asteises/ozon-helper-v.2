143dbc5919a6547bad2f49bf7a4d0a459f070170
f.nikitaev <f.nikitaev@bftcom.com>
2025-07-21 15:46:44 +0300

    init commit

---
+++ src/main/java/ru/asteises/ozonhelper/OzonHelperApplication.java
@@ -0,0 +1,13 @@
+package ru.asteises.ozonhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OzonHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OzonHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/asteises/ozonhelper/OzonHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.asteises.ozonhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OzonHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
