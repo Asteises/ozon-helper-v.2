0931bf19c2259f73fa528e0ae238f714ced7aaa2
f.nikitaev <f.nikitaev@bftcom.com>
2025-08-06 18:16:58 +0300

    Изменил настройки context-path

--- src/main/java/ru/asteises/ozonhelper/config/WebClientConfig.java
+++ src/main/java/ru/asteises/ozonhelper/config/WebClientConfig.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebClientConfig {
+public class WebClientConfig implements WebMvcConfigurer {
 
     @Value("${ozon.api.url}")
     private String baseUrl;
@@ -16,5 +18,12 @@ public class WebClientConfig {
         return builder
                 .baseUrl(baseUrl)
                 .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/miniapp/index.html")
+                .addResourceLocations("classpath:/static/miniapp/index.html")
+                .setCachePeriod(0);
     }
 }
